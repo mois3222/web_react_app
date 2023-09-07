96c576c3c2e3987f43321938ab1b661b12fe1509
mois3222 <moisky222@gmail.com>
2023-09-06 21:47:28 -0500

    slider done

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,39 @@
+import { useEffect, useState } from "react";
+import TGames from "../modules/games";
+
+type TGamesArr = Record<"games", TGames[]>;
+
 const Home = () => {
+  const [game, setGames] = useState<TGamesArr>();
+
+  useEffect(() => {
+    (async () => {
+      const rta: TGamesArr = await fetch("./src/API/db.json")
+        .then((res) => {
+          if (!res.ok) throw new Error().message;
+          return res.json();
+        })
+        .then((data) => data)
+        .catch(() => {});
+
+      setGames(rta);
+    })();
+  }, []);
+
+  const gameList = game?.games;
   return (
-    <>
-      <h1>Home</h1>
-    </>
+    <section className="Main__home">
+      <div className="Main__slider">
+        {gameList?.map((game, index) => (
+          <img
+            key={index}
+            src={game.img}
+            alt={game.name}
+            className={game.name}
+          />
+        ))}
+      </div>
+    </section>
   );
 };
 
